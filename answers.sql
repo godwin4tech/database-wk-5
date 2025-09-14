96ed82b3768f605cb0defa3e0a26b1bebb8ca003
godwin <kipkoechgodwin523@gmail.com>
2025-09-14 11:13:07 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,15 @@
+-- Question 1 🗑️
+-- Drop an index named IdxPhone from the customers table
+DROP INDEX IdxPhone ON customers;
+
+-- Question 2 👤
+-- Create a user named 'bob' with password 'S$cu3r3!' restricted to localhost
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Question 3 🔑
+-- Grant INSERT privilege to user 'bob' on the salesDB database
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- Question 4 🔐
+-- Change the password for the user 'bob' to 'P$55!23'
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
